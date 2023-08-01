86ba66481b2bb28148cf410fc72a9d9c4ca9f97e
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-08-01 23:35:17 +0900

    調整影なし

--- ShadowMap.cpp
+++ ShadowMap.cpp
@@ -39,12 +39,12 @@ static const std::string mesh_name = "Amago0.obj";
 MeshData mesh_data;
 
 glm::mat4 V_cam = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-glm::mat4 P_cam = glm::perspective(40.0f, 1.0f, 0.1f, 10.0f);
+glm::mat4 P_cam = glm::perspective(60.0f, 1.0f, 0.1f, 30.0f);
 glm::mat4 V_light = glm::lookAt(glm::vec3(0.0f, 2.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-glm::mat4 P_light = glm::perspective(40.0f, 1.0f, 0.1f, 10.0f);
+glm::mat4 P_light = glm::perspective(60.0f, 1.0f, 0.1f, 30.0f);
 
-//glm::mat4 M_fish;
-glm::mat4 M_quad = glm::translate(glm::vec3(0.0f, -0.5f, 0.0f))*glm::rotate(-45.0f, glm::vec3(1.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(0.75f));
+glm::mat4 M_fish;
+glm::mat4 M_quad = glm::translate(glm::vec3(0.0f, -0.8f, 0.0f))*glm::rotate(-45.0f, glm::vec3(1.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(0.75f));
 
 int render_mode = 1;
 
@@ -55,8 +55,8 @@ void draw_scene(glm::mat4& V)
    int VM_loc = glGetUniformLocation(shader_program, "VM");
    if(VM_loc != -1)
    {
-     /* const glm::mat4 VM = V*M_fish;*/
-      /*glUniformMatrix4fv(VM_loc, 1, false, glm::value_ptr(VM));*/
+      const glm::mat4 VM = V*M_fish;
+      glUniformMatrix4fv(VM_loc, 1, false, glm::value_ptr(VM));
    }
    glBindVertexArray(mesh_data.mVao);
 	glDrawElements(GL_TRIANGLES, mesh_data.mNumIndices, GL_UNSIGNED_INT, 0);
@@ -132,13 +132,13 @@ void draw_pass_2()
       glUniformMatrix4fv(P_loc, 1, false, glm::value_ptr(P_cam));
    }
 
-   const int Shadow_loc = glGetUniformLocation(shader_program, "Shadow");
-   const glm::mat4 S = glm::translate(glm::vec3(0.5f)) * glm::scale(glm::vec3(0.5f));
-   if(Shadow_loc != -1)
+   /*const int Shadow_loc = glGetUniformLocation(shader_program, "Shadow");
+   const glm::mat4 S = glm::translate(glm::vec3(0.5f)) * glm::scale(glm::vec3(0.5f));*/
+  /* if(Shadow_loc != -1)
    {
       const glm::mat4 Shadow = S*P_light*V_light*glm::inverse(V_cam); 
       glUniformMatrix4fv(Shadow_loc, 1, false, glm::value_ptr(Shadow));
-   }
+   }*/
 
    draw_scene(V_cam);
 }
@@ -181,7 +181,7 @@ void idle()
    const int time_ms = glutGet(GLUT_ELAPSED_TIME);
    float time_sec = 0.001f*time_ms;
 
-  /* M_fish = glm::rotate(15.0f*time_sec, glm::vec3(1.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(mesh_data.mScaleFactor));*/
+   //M_fish = glm::rotate(15.0f*time_sec, glm::vec3(1.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(mesh_data.mScaleFactor));
    
 }
 
@@ -301,7 +301,7 @@ void initOpenGl()
    reload_shader();
 
    mesh_data = LoadMesh(mesh_name);
-   /*M_fish = glm::scale(glm::vec3(mesh_data.mScaleFactor));*/
+   M_fish = glm::scale(glm::vec3(mesh_data.mScaleFactor));
 
    quad_vbo = -1;
 
