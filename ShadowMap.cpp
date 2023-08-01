fc72a1c442ea9c2741d6925d04aa1da958a7cd35
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-08-02 00:05:02 +0900

    ライトなし

--- ShadowMap.cpp
+++ ShadowMap.cpp
@@ -40,8 +40,6 @@ MeshData mesh_data;
 
 glm::mat4 V_cam = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 glm::mat4 P_cam = glm::perspective(100.0f, 1.0f, 0.1f, 30.0f);
-//glm::mat4 V_light = glm::lookAt(glm::vec3(0.0f, 2.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-//glm::mat4 P_light = glm::perspective(60.0f, 1.0f, 0.1f, 30.0f);
 
 glm::mat4 M_fish;
 glm::mat4 M_quad = glm::translate(glm::vec3(0.0f, -0.8f, 0.0f))*glm::rotate(-20.0f, glm::vec3(0.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(2.0f));
@@ -81,15 +79,10 @@ void draw_pass_1()
    }
   
    const int P_loc = glGetUniformLocation(shader_program, "P");
-   if(P_loc != -1)
-   {
-      //glUniformMatrix4fv(P_loc, 1, false, glm::value_ptr(P_light));
-   }
 
    glEnable(GL_POLYGON_OFFSET_FILL);
    glPolygonOffset(50.0, 30.0); 
 
-   //draw_scene(V_light);
    glDisable(GL_POLYGON_OFFSET_FILL);
 }
 
@@ -134,11 +127,6 @@ void draw_pass_2()
 
    const int Shadow_loc = glGetUniformLocation(shader_program, "Shadow");
    const glm::mat4 S = glm::translate(glm::vec3(0.5f)) * glm::scale(glm::vec3(0.5f));
-   if(Shadow_loc != -1)
-   {
-      //const glm::mat4 Shadow = S*P_light*V_light*glm::inverse(V_cam); 
-      //glUniformMatrix4fv(Shadow_loc, 1, false, glm::value_ptr(Shadow));
-   }
 
    draw_scene(V_cam);
 }
@@ -179,10 +167,7 @@ void idle()
 	glutPostRedisplay();
 
    const int time_ms = glutGet(GLUT_ELAPSED_TIME);
-   float time_sec = 0.001f*time_ms;
-
-   //M_fish = glm::rotate(15.0f*time_sec, glm::vec3(1.0f, 0.0f, 0.0f))*glm::scale(glm::vec3(mesh_data.mScaleFactor));
-   
+   float time_sec = 0.001f*time_ms;   
 }
 
 void reload_shader()
